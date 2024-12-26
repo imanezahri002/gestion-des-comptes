132d784f814dc42d85f6a9236e7dbe03a449c59a
imanezahri002 <imanezh5@gmail.com>
2024-12-26 15:05:23 +0100

    creation two table

--- db.sql
+++ db.sql
@@ -1,2 +1,14 @@
 CREATE DATABASE NeoBank;
 USE  NeoBank;
+
+CREATE TABLE account(
+    accountNum INT PRIMARY KEY AUTO_INCREMENT,
+    titulaire VARCHAR(50) NOT NULL,
+    soldeInit DECIMAL(15,2) NOT NULL
+    );
+CREATE TABLE savingAccount( 
+    	id INT PRIMARY KEY AUTO_INCREMENT,
+      	minimumSolde DECIMAL(5, 2) NOT NULL,
+       	accountNum INT,
+      FOREIGN KEY(accountNum) REFERENCES account(accountNum) ON DELETE CASCADE
+ );
